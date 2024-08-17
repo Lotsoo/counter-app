87b2b009d10eb107525b2bb1c9e91513da6b0bfb
lotsoo <braindeathrama@gmail.com>
2024-08-17 20:08:26 +0700

    feat: Added Column & Text

--- app/src/main/java/com/example/counterapp/MainActivity.kt
+++ app/src/main/java/com/example/counterapp/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.example.counterapp.ui.theme.CounterAppTheme
 
@@ -29,5 +33,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CounterApp(modifier: Modifier = Modifier) {
-
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text = "0")
+    }
 }
