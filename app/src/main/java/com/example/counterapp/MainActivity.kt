f0836344dc14b62fda9dcc68a99debc5342408be
lotsoo <braindeathrama@gmail.com>
2024-08-17 20:29:43 +0700

    feat: make the screen more beautiful

--- app/src/main/java/com/example/counterapp/MainActivity.kt
+++ app/src/main/java/com/example/counterapp/MainActivity.kt
@@ -7,9 +7,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,6 +25,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.counterapp.ui.theme.CounterAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -45,19 +53,33 @@ fun CounterApp(modifier: Modifier = Modifier) {
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize()
+        modifier = modifier.fillMaxSize()
     ) {
-        Text(text = counter.toString())
+        Text(text = counter.toString(), fontSize = 30.sp)
+        Spacer(modifier = Modifier.height(20.dp))
         Row {
-            ButtonCounter(type = { counter-- }, nameOfButton = "Decrease")
-            ButtonCounter(type = { counter++ }, nameOfButton = "Increase")
+            ButtonCounter(
+                type = { counter-- },
+                nameOfButton = "Decrease",
+                colorOfButton = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.error)
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            ButtonCounter(
+                type = { counter++ },
+                nameOfButton = "Increase",
+                colorOfButton = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.tertiary)
+            )
         }
     }
 }
 
 @Composable
-fun ButtonCounter(type: () -> Unit, nameOfButton: String, modifier: Modifier = Modifier) {
-    Button(onClick = type) {
+fun ButtonCounter(
+    type: () -> Unit,
+    nameOfButton: String,
+    colorOfButton: ButtonColors,
+) {
+    Button(onClick = type, colors = colorOfButton) {
         Text(text = nameOfButton)
     }
 }
